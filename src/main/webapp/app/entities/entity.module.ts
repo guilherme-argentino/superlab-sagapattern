c08551e8469d4aa6f76abb11c595e8a883d8a353
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2020-04-15 09:02:49 -0300

    Redis

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,38 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'region',
+        loadChildren: () => import('./region/region.module').then(m => m.SuperLabSagaPatternRegionModule)
+      },
+      {
+        path: 'country',
+        loadChildren: () => import('./country/country.module').then(m => m.SuperLabSagaPatternCountryModule)
+      },
+      {
+        path: 'location',
+        loadChildren: () => import('./location/location.module').then(m => m.SuperLabSagaPatternLocationModule)
+      },
+      {
+        path: 'department',
+        loadChildren: () => import('./department/department.module').then(m => m.SuperLabSagaPatternDepartmentModule)
+      },
+      {
+        path: 'task',
+        loadChildren: () => import('./task/task.module').then(m => m.SuperLabSagaPatternTaskModule)
+      },
+      {
+        path: 'employee',
+        loadChildren: () => import('./employee/employee.module').then(m => m.SuperLabSagaPatternEmployeeModule)
+      },
+      {
+        path: 'job',
+        loadChildren: () => import('./job/job.module').then(m => m.SuperLabSagaPatternJobModule)
+      },
+      {
+        path: 'job-history',
+        loadChildren: () => import('./job-history/job-history.module').then(m => m.SuperLabSagaPatternJobHistoryModule)
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ]
